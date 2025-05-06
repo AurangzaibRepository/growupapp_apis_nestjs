5d26701830e6436e419c7b7aa37182b24fdbb318
Aurangzaib <aurangzaib_najam@hotmail.com>
2025-05-06 22:08:41 +0500

    Creates config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { Dialect } from 'sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+    }),
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_DIALECT as Dialect,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '3306'),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DN_NAME,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/configuration.ts
@@ -0,0 +1,13 @@
+export default () => ({
+  app: {
+    name: process.env.APP_NAME,
+    port: process.env.APP_PORt,
+  },
+  database: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+});
